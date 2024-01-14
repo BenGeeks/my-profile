60719ac483fca33dbf5dd580c1e6d3db9af16332
Ben Gican <b3nhur77@gmail.com>
2024-01-15 02:00:26 +0800

    added soon to rise

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+        <h1>SOON TO RISE</h1>
       </div>
     </main>
   );
