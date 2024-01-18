f6630057be336f17f354e35b39e387ce4f942660
Ben Gican <b3nhur77@gmail.com>
2024-01-18 19:17:50 +0800

    try again and again and again and again and again

--- src/app/page.js
+++ src/app/page.js
@@ -8,25 +8,28 @@ export default function Home() {
     <>
       <div className={styles.description}>
         <p>
-          Meet a resourceful and self-taught web developer with a stellar reputation for getting the job done. With over 4 years of
-          programming experience, he specializes in React.js front-end development, as well as Next.js and MERN full-stack development.
+          {`
+        Meet a resourceful and self-taught web developer with a stellar reputation for getting the job done. With over 4 years of
+        programming experience, he specializes in React.js front-end development, as well as Next.js and MERN full-stack development.`}
         </p>
         <br />
         <p>
-          Having earned a Top Rated Plus rating on Upwork, this developer has successfully delivered high-quality projects, demonstrating a
-          commitment to excellence. With a background encompassing 15 years in technical support, he brings a wealth of experience in
-          assisting and troubleshooting users' concerns and issues. Additionally, boasting 6 years of thriving in a work-from-home
-          environment, his adaptability and discipline shine through.
+          {`
+        Having earned a Top Rated Plus rating on Upwork, this developer has successfully delivered high-quality projects, demonstrating a
+        commitment to excellence. With a background encompassing 15 years in technical support, he brings a wealth of experience in
+        assisting and troubleshooting users' concerns and issues. Additionally, boasting 6 years of thriving in a work-from-home
+        environment, his adaptability and discipline shine through.`}
         </p>
         <br />
         <p>
-          Beyond his development expertise, he possesses 5 years of experience in QA testing, covering both manual and automated methods.
-          This comprehensive skill set ensures the quality and reliability of the applications he works on.
+          {`
+        Beyond his development expertise, he possesses 5 years of experience in QA testing, covering both manual and automated methods.
+        This comprehensive skill set ensures the quality and reliability of the applications he works on.`}
         </p>
         <br />
         <p>
-          Passionate about creating seamless and visually appealing web experiences, this developer is eager to contribute his skills to
-          your projects. Consider connecting to explore how his diverse background and expertise can benefit your team!
+          {`Passionate about creating seamless and visually appealing web experiences, this developer is eager to contribute his skills to
+          your projects. Consider connecting to explore how his diverse background and expertise can benefit your team!`}
         </p>
       </div>
 
